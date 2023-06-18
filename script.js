f092199ad0ee8b41c1c74c1c6fcaff9b7ac13a89
mannmanjeet988 <120735535+mannmanjeet988@users.noreply.github.com>
2023-06-18 11:56:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+function removeArticles(touristSpots) {
+  const articles = ['The', 'A', 'An'];
+  const filteredSpots = touristSpots.filter((spot) => {
+    const firstWord = spot.split(' ')[0];
+    return !articles.includes(firstWord);
+  });
+  return filteredSpots;
+}
 
+
+
+   console.log (filteredSpots.map(removeArticles)
+    .sort((a, b) => a.localeCompare(b));)
