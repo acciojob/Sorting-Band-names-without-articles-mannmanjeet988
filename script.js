70476a4065a50c1a158ddb0bb8bb995bf5378616
mannmanjeet988 <120735535+mannmanjeet988@users.noreply.github.com>
2023-06-18 15:48:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-//your code here
-function removeArticles(touristSpots) {
-  const articles = ['The', 'A', 'An'];
-  const filteredSpots = touristSpots.filter((spot) => {
-    const firstWord = spot.split(' ')[0];
-    return !articles.includes(firstWord);
+let bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Rolling Stones', 'Queen'];
+
+function sortBandNames(bandNames) {
+  const articles = ['a', 'an', 'the'];
+
+  // Remove articles from band names
+  const cleanedNames = bandNames.map(name => {
+    // Split the name into individual words
+    const words = name.split(' ');
+
+    // Filter out the articles
+    const filteredWords = words.filter(word => !articles.includes(word.toLowerCase()));
+
+    // Join the remaining words back together
+    const cleanedName = filteredWords.join(' ');
+
+    return cleanedName;
   });
-  return filteredSpots;
+
+  // Sort the band names in lexicographic order
+  const sortedNames = cleanedNames.sort((a, b) => a.localeCompare(b));
+
+  // Display the sorted names in the HTML page
+  const ulElement = document.getElementById('band');
+  sortedNames.forEach(name => {
+    const liElement = document.createElement('li');
+    liElement.textContent = name;
+    ulElement.appendChild(liElement);
+  });
 }
 
-
-
-   console.log (touristSpots.map(removeArticles)
-    .sort((a, b) => a.localeCompare(b));)
+// Call the function with the band names array
+sortBandNames(bandNames);
